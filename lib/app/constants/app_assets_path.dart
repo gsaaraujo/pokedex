eed0fd11e72bde46406096d62635ed0b833d3fb1
Gabriel Araujo <gabriel.houth@gmail.com>
2021-09-23 19:29:22 -0300

    feat: add image

--- lib/app/constants/app_assets_path.dart
+++ lib/app/constants/app_assets_path.dart
@@ -1,5 +1,6 @@
 class AppAssetsPath {
   static const defaultUserPhotoJpg = "assets/images/defaultUserPhoto.jpg";
   static const logoPng = "assets/images/logo.pgn";
-  static const pokeballPng = "assets/images/pokeball.png";
+  static const pokeballSvg = "assets/images/pokeball.svg";
+  static const bubblesSvg = "assets/images/bubbles.svg";
 }
